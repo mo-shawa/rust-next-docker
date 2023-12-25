3b90f491423390e50b4e5485e2acb2ff36c367c7
Mahmoud Shawa <mahmoud@shawa.dev>
2023-12-25 21:39:44 +0300

    add put handler

--- server/src/main.rs
+++ server/src/main.rs
@@ -157,3 +157,21 @@ fn handle_get_all_request(request: &str) -> (String, String){
     }
 }
 
+fn handle_put_request(request: &str) -> (String, String) {
+    match(
+        get_id(&request).parse::<i32>(), 
+        get_user_request_body(request),
+        Client::connect(DB_URL, NoTls)
+    ){
+        (Ok(id), Ok(user), Ok(mut client)) => {
+            client.execute("UPDATE users SET name = $1, email = $2 WHERE id = $3", &[&user.name, &user.email, &id])
+            .unwrap();
+
+            return (OK_RESPONSE.to_string(), "User updated".to_string())
+        }
+       _ => return (INTERNAL_SERVER_ERROR_RESPONSE.to_string(), "Internal error".to_string())
+    }
+}
+
+
+
